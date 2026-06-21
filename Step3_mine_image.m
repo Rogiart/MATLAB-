0f0edfcfb446b64f1bf4e4aabfe95b403d6e237f
MASART <c1406436@st.kanazawa-it.ac.jp>
2026-06-21 17:54:07 +0900

    Add files via upload

---
+++ Step3_mine_image.m
@@ -0,0 +1,73 @@
+%% 手書き文字から学習用データの作成
+clear all;clc;close all;[status] = rmdir('image_mine','s');
+%% スキャナーで読み取った100個の手書き文字が書かれた画像を読み込み
+I = imread('image_mine.jpg');
+figure,imshow(I);
+%% 数値が書かれた領域のボックスを作成
+BW = imbinarize(I); % 二値化
+BW = imclearborder(BW); % 周囲に接している部分を
+BWbbox = imfill(BW,'holes'); % 穴を埋める
+BWbbox = imclose(BWbbox,ones(30)); % クローズ処理
+BWbbox = bwareaopen(BWbbox,100); % 100より小さいごみを処理
+% figure, imshow(BWbbox);
+%% 各数字をトリミングで切り出しフォルダに保存
+% 各数字の領域の範囲を指定
+thresh = [0 320 520 720 920 1100 1280 1480 1680 1880 2080];
+% 各数字の位置とサイズの情報を取得
+statsbbox = regionprops('table',BWbbox,'Centroid','BoundingBox');
+% 数字の種類ごとに処理
+for n= 1:10
+    % 数字nの場所だけを取り出す
+    idx = statsbbox.Centroid(:,2) < thresh(n+1) & statsbbox.Centroid(:,2) > thresh(n);
+    % 各数字のエリアの中心座標を取り出す
+    r = statsbbox.Centroid(idx,2); 
+    c = statsbbox.Centroid(idx,1);
+    % 取り出すエリアの情報とサイズの調整
+    rect = statsbbox.BoundingBox(idx,:);
+    rect(:,1) = rect(:,1)+ 5;
+    rect(:,2) = rect(:,2)+ 5;
+    rect(:,3) = rect(:,3)- 10;
+    rect(:,4) = rect(:,4)- 10;
+    BW2 = bwselect(BWbbox,c,r,4);
+    % 保存用のディレクトリを作成
+    if n == 10 
+        mkdir image_mine\train\0
+        mkdir image_mine\test\0
+    else
+        mkdir(['image_mine\train\' num2str(n)])
+        mkdir(['image_mine\test\' num2str(n)])
+    end
+    % 各種10個ずつ数値があるので、トリミングし輝度反転し名前を付けて保存
+    for i=1:10
+        BW3 = bwselect(BW2,c(i),r(i),4); % 10個のうち一つを指定
+        I2 = imcrop(I,rect(i,:)); % 指定領域をトリミング
+        I3 = imadjust(255 - I2); % 輝度反転し、正規化
+        I4 = imresize(I3,[28 28]); % サイズを28ｘ28に変換
+        % 各フォルダに保存
+        % 一枚ずつ表示
+        imshow(I4);shg;
+        if i < 6 % 5番目までは学習用
+            ftype = 'train';
+            fnum=i;
+        else % 6番目以降は評価用
+            ftype = 'test';
+            fnum=i-5;
+        end
+        % 画像を保存
+        if n ==10
+            imwrite(I4,['image_mine\' ftype '\0' '\' ftype '_' num2str(0) '_' num2str(fnum) '_mine.jpg'] );
+        else
+            imwrite(I4,['image_mine\' ftype '\' num2str(n) '\' ftype '_' num2str(n) '_' num2str(fnum) '_mine.jpg'] );
+        end
+        clear I2 I3 
+    end
+end
+winopen('image_mine') % Windowsのエクスプローラでフォルダを開く
+%% 
+function buttonaction
+    if isempty(txt.String)
+    else
+        b =false;
+    end
+end
+% Copyright 2019 The MathWorks, Inc.
---
+++ Step7_mark_CNN.m
@@ -0,0 +1,79 @@
+clear; clc; close all;
+
+%% 層の読み込みと画像のセット
+load mylayers
+% 学習用データの定義
+imdshandTrain = imageDatastore('image_mark\train', 'IncludeSubfolders',true,'LabelSource','foldernames');
+% テスト用データの定義
+imdshandValidation = imageDatastore('image_mark\test', 'IncludeSubfolders',true,'LabelSource','foldernames');
+%% 学習オプションの設定
+options = trainingOptions('sgdm', ... % 最適化エンジン。確率的勾配降下法を選択
+    'MaxEpochs',1, ... % 学習のデータセットの繰り返し頻度
+    'ValidationData',imdshandValidation, ... % 評価用データの指定
+    'ValidationFrequency',10, ... % 評価用データでの検証頻度
+    'Verbose',false); % 
+
+%% 学習と結果の可視化、プロット作成
+k=30; % 学習の繰り返し回数(Epoch)
+f = figure; % プロットの作成
+f.Units = 'normalized';
+f.Position = [0.05 0 0.5 0.8];shg
+% 変数の初期値設定
+% 精度が指定の値を超えたかチェックするフラグを指定
+flag25 =1;
+flag50 =1;
+flag75 =1;
+m =zeros(4,1);
+for nn = 1:k
+    if nn == 1
+        net = trainNetwork(imdshandTrain,layers,options); % 学習
+    else
+        net = trainNetwork(imdshandTrain,net.Layers,options); % 学習
+    end
+    YPred = classify(net,imdshandValidation);% 予測
+    for ii = 1:numel(imdshandValidation.Labels)  
+        Itest = read(imdshandValidation); % 画像の読み込み
+        YPred1 = classify(net,Itest); % 画像ごとに予測
+        Itest3 = cat(3,Itest,Itest,Itest); % 画像をカラーに変換
+        if ~(YPred1 == imdshandValidation.Labels(ii))
+            Color = 'red'; % 予測結果が間違っていたら赤
+        else
+            Color = 'green'; % 予測が正しければ緑
+        end
+        Itest3 = insertShape(Itest3,'Rectangle',[1 1 28 28],...
+            'Color', Color,'LineWidth',3); % 画像の周囲に指定した枠をつける
+        data{ii} = Itest3; % 枠が付いたあとの画像をdataに格納
+    end
+    reset(imdshandValidation); % imagedatastoreの読み出しをリセット
+    subplot(1,2,1), montage(data,'Size',[10 5]); % 各画像の判定結果をfigureの左側に表示
+    YValidation = imdshandValidation.Labels;
+    accuracy(nn) = sum(YPred == YValidation)/numel(YValidation);% 現時点での精度を計算
+    % 精度が25%,50%,75%を超えた直後のファイルのみ変数として保持
+    if accuracy(nn) < 0.25 
+    elseif accuracy(nn) >0.25 & flag25 ==1 % 25%を超えたとき
+        net25 = net;
+        m(1) =nn;
+        flag25=0;
+    elseif accuracy(nn) > 0.5 & flag50 ==1 % 50%を超えたとき
+        net50 = net;
+        m(2) =nn;
+        flag50=0;
+    elseif accuracy(nn) > 0.75 & flag75 ==1 % 75%を超えたとき
+        net75 = net;
+        m(3) =nn;
+        flag75 =0;      
+    end
+    subplot(1,2,2), plot(accuracy*100,'blue -','LineWidth',2),ylim([0 100]) % 精度をfigureの右側に表示
+    axis([0 k 0 100]);
+    title('Machine Learning','FontSize',12,'Interpreter','latex','FontName','Euclid')% タイトル
+    xlabel('Iteration','FontSize',12,'Interpreter','latex','FontName','Euclid');% x座標ラベル
+    ylabel('Accuracy','FontSize',12,'Interpreter','latex','FontName','Euclid'); % y座標ラベル
+    shg;
+end
+accuracyfin = accuracy(end)*100; 
+disp(['Accuracy of machine learning mode is ',num2str(accuracyfin),' %.']); % 精度の表示
+m(4) = nn(end);
+% 精度の曲線、4つのネットワーク、保存した回数の番号
+save('netresult_mark.mat','net*','accuracy','m')
+
+% Copyright 2019 The MathWorks, Inc.
